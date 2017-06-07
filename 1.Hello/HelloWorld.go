db45a9d4af9a986bc2ce89dbf214ea87aa1de537
pawan9489 <pawankumar.gubbala@gmail.com>
2017-06-07 22:04:07 +0530

    factorial

--- 1.Hello/HelloWorld.go
+++ 1.Hello/HelloWorld.go
@@ -3,10 +3,29 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Pawan Kumar")
-	fmt.Print("wordff")
-	fmt.Print("wordff")
-	fmt.Print("wordff")
-	fmt.Printf("Wtf")
-	fmt.Printf("Wtf")
+	fmt.Printf("The factorial is %d", fact(60))
+}
+
+func factorial(num int) int {
+	if num <= 1 {
+		return num
+	}
+	return num * factorial(num - 1)
+}
+
+func fact(num int) int{
+	//ans := 1
+	//func helper(acc int, y int) int {
+	//	if y == 1{
+	//		return acc
+	//	}
+	//	return helper(acc * y, y-1)
+	//}
+	return helper2(1,num)
+}
+func helper2(acc int, y int) int {
+	if y == 1{
+		return acc
+	}
+	return helper2(acc * y, y-1)
 }
