c74e417883492aa283d9b7cf80ca82c31ec9a451
pawan9489 <pawankumar.gubbala@gmail.com>
2017-06-14 20:20:09 +0530

    Control Flow

--- 1.Hello/LangSpec2.go
+++ 1.Hello/LangSpec2.go
@@ -1,8 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main()  {
-	a := 42
-	fmt.Println(&a)
+	//a := 42
+	//fmt.Println(&a)
+	//var b* int = &a
+	//fmt.Println(b)
+	//var c *int = &a
+	//fmt.Println(c)
+	//
+	//fmt.Println(*b)
+	//fmt.Println(*c)
+	//*b = 55
+	//fmt.Println(*b)
+	//fmt.Println(*c)
+	//fmt.Println(a)
+
+	//for pos, char := range "日本\x80語" { // \x80 is an illegal UTF-8 encoding
+	//	fmt.Printf("character %#U starts at byte position %d\n", char, pos)
+	//}
+	////}
+
+	switch "Pawan" {
+	case "kumar":
+	case "Pawan":
+		fmt.Println("First Name")
+		fmt.Println("Multi Line without braces are allowed")
+		fallthrough
+	default:
+		fmt.Println("default")
+	}
+
+	switch "OO" {
+	case "PK", "OK":
+		fmt.Println("PK or OK")
+	case "OO":
+		fmt.Println("OO")
+	}
+
 }
